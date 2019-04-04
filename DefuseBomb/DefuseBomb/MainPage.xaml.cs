4aa7218ea2cb55fe3cbf3fd75ba00273930085cf
NassimaMsaddak <nassimamsaddak2023@gmail.com>
2019-04-04 19:07:14 +0200

    Defuse Bomb

--- DefuseBomb/DefuseBomb/MainPage.xaml.cs
+++ DefuseBomb/DefuseBomb/MainPage.xaml.cs
@@ -9,9 +9,31 @@ namespace DefuseBomb
 {
     public partial class MainPage : ContentPage
     {
+
+        static string bomb = new Random().Next(1, 4).ToString();
+        static int scores = 0;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void ButtonClicked(object sender,EventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button.Text == bomb)
+            {
+                await DisplayAlert("Bomb Exploded", "Game Over", "Retry");
+                bomb = new Random().Next(1, 4).ToString();
+                scores = 0;
+
+            } else {
+                scores += 1;
+                await DisplayAlert("Bomb Defused !", "Scores: "+ scores , "Continue");
+                bomb = new Random().Next(1, 4).ToString();
+            }
+
         }
     }
 }
